023047eb72d6b124a977dd344dea63cd545a5d8a
Quinten Kamphuis <163667554+quinten-kamphuis@users.noreply.github.com>
2024-04-24 18:20:53 +0200

    Add game logic, finish screen and update variables

--- script.js
+++ script.js
@@ -4,42 +4,13 @@ const timerDiv = document.getElementById('timer');
 
 const flexContainer = document.getElementById('flex-container');
 const pictureArr = [];
+
 let cardsClickable = true;
 let isFirst = true;
 let lastClicked = 0;
 let lastClickedIndex = 0;
-let numOfSets = 0;
-
-const addPicturesToCards = () => {
-    console.table(pictureArr);
-};
-
-const makePictures = (pictures) => {
-    if (pictures > 9) {
-        console.error('Too many cards for algorithm, infinite loop!');
-        return;
-    }
-    while(pictureArr.length < pictures*2){
-        const picture = Math.floor(Math.random() * 9 + 1);
-        if (!pictureArr.includes(picture)){
-            pictureArr.push(picture, picture);
-        }
-    }
-    pictureArr.sort(() => Math.random() - 0.5);
-    addPicturesToCards();
-};
-
-const makeCards = (cards) => {
-    if (cards % 2 === 1){
-        console.warn("The number of cards must be even! It's corrected down.") 
-        cards--;
-    }
-    flexContainer.innerHTML = ``;
-    for(i = 1; i <= cards; i++){
-        flexContainer.innerHTML += `<div id="card-${i}"class="card unset"></div>`;
-    }
-    makePictures(cards/2);
-};
+let numOfSetsFound = 0;
+let numOfCards = 12;
 
 const changeAllCards = (cursor, content) => {
     const cards = document.querySelectorAll('.card.unset');
@@ -63,25 +34,65 @@ const disableCards = () => {
         }
         changeAllCards('pointer', null)
         isFirst = true;
-    },1000);
+    },1200);
+};
+
+const roundWon = () => {
+    flexContainer.innerHTML = `<h1>You Won!</h1>`;
+};
+
+const checkForSet = (cardNum) => {
+    if(!isFirst && lastClicked === pictureArr[cardNum]){
+        document.getElementById(`card-${cardNum+1}`).className = 'card set';
+        document.getElementById(`card-${lastClickedIndex+1}`).className = 'card set';
+        numOfSetsFound++;
+        if(numOfSetsFound === numOfCards/2){
+            roundWon();
+        }
+    }
 };
 
 const showCard = (cardDiv, cardNum) => {
     if(cardDiv.innerText) return;
+    console.log(numOfSetsFound, numOfCards)
     if(cardsClickable){
+        disableCards();
+        checkForSet(cardNum);
         cardDiv.innerText = pictureArr[cardNum];
-        if(!isFirst && lastClicked === pictureArr[cardNum]){
-            document.getElementById(`card-${cardNum+1}`).className = 'card set';
-            document.getElementById(`card-${lastClickedIndex+1}`).className = 'card set';
-        }
         lastClicked = pictureArr[cardNum];
         lastClickedIndex = cardNum;
-        disableCards();
     }
 };
 
+const makePictures = (pictures) => {
+    if (pictures > 9) {
+        console.error('Too many cards for algorithm, infinite loop!');
+        return;
+    }
+    while(pictureArr.length < pictures*2){
+        const picture = Math.floor(Math.random() * 9 + 1);
+        if (!pictureArr.includes(picture)){
+            pictureArr.push(picture, picture);
+        }
+    }
+    pictureArr.sort(() => Math.random() - 0.5);
+};
+
+const makeCards = (cards) => {
+    if (cards % 2 === 1){
+        console.warn("The number of cards must be even! It's corrected down.") 
+        numOfCards--;
+        cards--;
+    }
+    flexContainer.innerHTML = ``;
+    for(i = 1; i <= cards; i++){
+        flexContainer.innerHTML += `<div id="card-${i}"class="card unset"></div>`;
+    }
+    makePictures(cards/2);
+};
+
 const startGame = () => {
-    makeCards(12);
+    makeCards(numOfCards);
     const cardDivs = flexContainer.children;
     for (let i = 0; i < cardDivs.length; i++){
         cardDivs[i].addEventListener('click', () => {
